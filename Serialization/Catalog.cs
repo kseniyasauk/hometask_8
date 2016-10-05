6c612ccb3e679c8e0a85c4b7a4d779ba2da633fa
kseniyasauk <kseniya_sauk@epam.com>
2016-10-05 16:12:04 +0300

    add task8_serialization

---
+++ Serialization/Catalog.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+
+namespace Serialization
+{
+    
+
+    [XmlType (TypeName = "catalog")]
+    public class Catalog
+    {
+        [XmlArray]
+        public List<Book> Books { get; set; }
+    }
+
+    [XmlType (TypeName = "book")]
+    public class Book
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlElement ("isbn")]
+        public string Isbn { get; set; }
+
+        [XmlElement("author")]
+        public string Author { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("genre")]
+        public Genre  Gen{ get; set; }
+
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+
+        [XmlElement("publish_date")]
+        public DateTime PublishDate { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("registration_date")]
+        public string RegistrationDate { get; set; }
+    }
+
+    public enum Genre
+    {
+        [XmlEnum]
+        Computer,
+        [XmlEnum]
+        Fantasy,
+        [XmlEnum]
+        Romance,
+        [XmlEnum]
+        Horror,
+        [XmlEnum]
+        ScienceFiction,
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = @"D:\file.txt"; 
+            Catalog c = new Catalog();
+
+            var serializer = new XmlSerializer(typeof(Catalog));
+            var stream = new FileStream(fileName, FileMode.Create);
+            serializer.Serialize(stream, c);
+            stream.Close();
+        }
+    }
+}
